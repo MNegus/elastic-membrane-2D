1f7d7eba2578e5363cb40443c91e050178499e73
Michael <michaelnegus1@gmail.com>
2021-05-13 17:51:57 +0100

    Minor changes

--- finite_difference/C_code/anim_all.gp
+++ finite_difference/C_code/anim_all.gp
@@ -1,7 +1,9 @@
 do for [ii=0:10000] {
         print ii
-       	plot 'explicit_outputs/w_'.ii.'.txt' with lines smooth unique, \
-             'implicit_outputs/w_'.ii.'.txt' with lines smooth unique, \
+       	# plot 'explicit_outputs/w_'.ii.'.txt' with lines smooth unique, \
+        #      'implicit_outputs/w_'.ii.'.txt' with lines smooth unique, \
+	#      'exact_outputs/w_'.ii.'.txt' with lines smooth unique
+        plot 'implicit_outputs/w_'.ii.'.txt' with lines smooth unique, \
 	     'exact_outputs/w_'.ii.'.txt' with lines smooth unique
         pause 0.001
 }
--- finite_difference/C_code/parameters.h
+++ finite_difference/C_code/parameters.h
@@ -3,14 +3,14 @@
 */
 
 /* Physical parameters */
-const double ALPHA = 0.1; // Mass term
+const double ALPHA = 1.0; // Mass term
 const double BETA = 10; // Tension term
 const double GAMMA = 1; // Bending stiffness term
 const double L = 4; // Width of domain in x
 
 
 /* Computational parameters */
-const int N_MEMBRANE = 64; // Number of grid points on the membrane
-const double T_MAX = 0.4; // Maximum value of time
-const double DELTA_T = 1e-4; // Timestep size
+const int N_MEMBRANE = 1024; // Number of grid points on the membrane
+const double T_MAX = 0.1; // Maximum value of time
+const double DELTA_T = 1e-5; // Timestep size
 
