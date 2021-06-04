0c9ef5fa317bd2db6c91ffa0434639bf74b69c7b
Michael <michaelnegus1@gmail.com>
2021-06-04 09:43:50 +0100

    FD algorithm testing

--- finite_difference_development/matlab_code/plot_exact.m
+++ finite_difference_development/matlab_code/plot_exact.m
@@ -1,36 +1,51 @@
 %% mitchell_method_comparison.m
 % Code to compare the Mitchell method scheme for the wave equation solution
 % to the exact solution
-
+close all;
+clear;
 %% Parameters
-ALPHA = 0.002;
-BETA = 7;
-GAMMA = 0.0001;
-% GAMMA = 0;
 L = 4;
-N_MEMBRANE = 128;
-T_MAX = 0.1;
-DELTA_T = 1e-4;
+N_MEMBRANE = 8192;
+Deltaxs =  L ./ (N_MEMBRANE - 1)
+T_MAX = 0.4;
+DELTA_T = 1e-3;
+
+% ALPHA = 1;
+% BETA = ALPHA * (L / (5461 - 1))^2 / ((1e-4)^2)
+% GAMMA = ALPHA * (L / (5461 - 1))^4 / ((1e-4)^2)
+ALPHA = 1; BETA = 1; GAMMA = 1;
+
+
 tvals = 0 : DELTA_T : T_MAX;
 xs = linspace(0, L, N_MEMBRANE);
 
 %% Numerically solves and compare
+writerobj = VideoWriter("exact_video.avi");
+open(writerobj);
 
+figure(1);
 for t = tvals
     t
     % Exact solution
     w_exact_1 = homogeneous_exact_solution(xs, t, ALPHA, BETA, GAMMA, L);
-    w_exact_2 = homogeneous_exact_solution(xs, t, ALPHA, BETA, 0, L);
+%     w_exact_2 = homogeneous_exact_solution(xs, t, ALPHA, BETA, 0, L);
 
     %  Plots full
-    figure(1);
-    plot(xs, w_exact_1);
-    hold on
-    plot(xs, w_exact_2);
-    hold off;
+    plot(xs, w_exact_1, 'linewidth', 2);
+    ylim([-0.02, 0.18]);
+    xlim([0, L]);
+    fontsize = 20;
+    set(gca,'TickLabelInterpreter','latex');
+    set(gca,'fontsize', fontsize);
+    xlabel("$x$", "interpreter", "latex", "fontsize", fontsize);
+    ylabel("$w(x, t)$", "interpreter", "latex", "fontsize", fontsize);
+    grid on;
 
+    frame = getframe(gcf);
+    writeVideo(writerobj,frame);
     pause(0.01);
 
 end
+close(writerobj);
 
 
