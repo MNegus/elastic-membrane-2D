806e99a3e42d63c7b4b2dc403221d0dadcd1366d
Michael <michaelnegus1@gmail.com>
2021-06-17 09:33:47 +0100

    Gets rid of sorting, just moves files into subdirectories

--- basilisk_scripts/utility_scripts/output_clean.sh
+++ basilisk_scripts/utility_scripts/output_clean.sh
@@ -8,18 +8,24 @@ PARENTDIR=$1 # Parent directory i.e. containing code/ and raw_data/
 
 cd $PARENTDIR
 
-mkdir pressure_outputs # Directory to store pressure output files
+mkdir boundary_outputs # Directory to store boundary output files
+mkdir membrane_outputs # Directory to store membrane outputs
 
 # Works out how many boundary_output_*.txt files there are in raw_data/
 NOFILES=$(ls raw_data/ | grep 'boundary_output_.*\.txt' | wc -l)
 
-# Loops over all boundary_output files, sorting them by x and outputting them
-# into the pressure_outputs directory
-for filenum in $(seq 0 $(($NOFILES - 1)))
-do
-    sort -g -k1 raw_data/boundary_output_$filenum.txt > pressure_outputs/p_$filenum.txt
-    echo Sorted boundary_output_$filenum.txt
-done
+# # Loops over all boundary_output files, sorting them by x and outputting them
+# # into the pressure_outputs directory
+# for filenum in $(seq 0 $(($NOFILES - 1)))
+# do
+#     sort -g -k1 raw_data/boundary_output_$filenum.txt > boundary_outputs/boundary_output_$filenum.txt
+#     echo Sorted boundary_output_$filenum.txt
+# done
+
+# Move files
+mv raw_data/boundary_output_*.txt boundary_outputs
+mv raw_data/w_*.txt membrane_outputs
+mv raw_data/p_*.txt membrane_outputs
 
 # Copies the log file into the upper directory 
 cp raw_data/log .
