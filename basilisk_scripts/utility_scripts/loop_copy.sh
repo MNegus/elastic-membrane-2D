223c3eace3e5ec16e516514378561363bd4eab46
Michael <michaelnegus1@gmail.com>
2021-06-25 13:53:53 +0100

    Setting up for gamma varying run

--- basilisk_scripts/utility_scripts/loop_copy.sh
+++ basilisk_scripts/utility_scripts/loop_copy.sh
@@ -3,12 +3,12 @@
 code_dir=$1
 dest_dir=$2
 
-for BETA in 0.01 0.1 1 10 100
+for GAMMA in 0.01 0.1 1 10 100
 do
     # Changes the parameters file
     # sed -i "/MAXLEVEL/c\const int MAXLEVEL = $MAXLEVEL; // Maximum refinement level" parameters.h
-    sed -i "/BETA/c\const double BETA = $BETA; // Tension term" parameters.h
+    sed -i "/GAMMA/c\const double GAMMA = $GAMMA; // Bending term" parameters.h
 
     # Copies over the code 
-    ./code_copy.sh $code_dir $dest_dir beta_$BETA
+    ./code_copy.sh $code_dir $dest_dir gamma_$GAMMA
 done
--- basilisk_scripts/utility_scripts/loop_run.sh
+++ basilisk_scripts/utility_scripts/loop_run.sh
@@ -4,14 +4,14 @@ parent_dir=$1
 code_name=$2
 
 
-for BETA in 100 10 1 0.1 0.01
+for GAMMA in 100 10 1 0.1 0.01
 do
     # echo Max level = $MAXLEVEL
     # cd $parent_dir/max_level_$MAXLEVEL-coupled_$COUPLED/code
     
-    echo BETA = $BETA
-    cd $parent_dir/beta_$BETA/code
+    echo GAMMA = $GAMMA
+    cd $parent_dir/gamma_$GAMMA/code
 
-    ./run_simulation.sh $code_name 8
+    ./run_simulation.sh $code_name 4
 
 done
