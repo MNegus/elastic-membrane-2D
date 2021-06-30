4d5047930b7956499c7f0b4ee523d7139214d096
Michael <michaelnegus1@gmail.com>
2021-07-01 00:14:02 +0100

    Loops ready for max level validatioN

--- basilisk_scripts/utility_scripts/loop_copy.sh
+++ basilisk_scripts/utility_scripts/loop_copy.sh
@@ -3,19 +3,28 @@
 code_dir=$1
 dest_dir=$2
 
-# for GAMMA in 0.01 0.1 1 10 100
-for XMIN in 0 1 2 3 4
+
+for MAXLEVEL in 8 9 10 11 12 13
 do
-    sed -i "/x_min_height/c\const int x_min_height = $XMIN;" parameters.h
-    for YMIN in 0 1 2 3
-    do
-        # Changes the parameters file
-        # sed -i "/MAXLEVEL/c\const int MAXLEVEL = $MAXLEVEL; // Maximum refinement level" parameters.h
-        sed -i "/y_min_height/c\const int y_min_height = $YMIN;" parameters.h
+    # Changes the parameters file
+    sed -i "/MAXLEVEL/c\const int MAXLEVEL = $MAXLEVEL; // Maximum refinement level" parameters.h
 
-        # Copies over the code 
-        ./code_copy.sh $code_dir $dest_dir x_min_$XMIN-y_min_$YMIN
-
-    done
-    
+    # Copies over the code 
+    ./code_copy.sh $code_dir $dest_dir max_level_$MAXLEVEL
 done
+
+# for XMIN in 0 1 2 3 4
+# do
+#     sed -i "/x_min_height/c\const int x_min_height = $XMIN;" parameters.h
+#     for YMIN in 0 1 2 3
+#     do
+#         # Changes the parameters file
+#         # sed -i "/MAXLEVEL/c\const int MAXLEVEL = $MAXLEVEL; // Maximum refinement level" parameters.h
+#         sed -i "/y_min_height/c\const int y_min_height = $YMIN;" parameters.h
+
+#         # Copies over the code 
+#         ./code_copy.sh $code_dir $dest_dir x_min_$XMIN-y_min_$YMIN
+
+#     done
+    
+# done
--- basilisk_scripts/utility_scripts/loop_run.sh
+++ basilisk_scripts/utility_scripts/loop_run.sh
@@ -4,28 +4,24 @@ parent_dir=$1
 code_name=$2
 
 
-# for GAMMA in 100 10 1 0.1 0.01
-# do
-#     # echo Max level = $MAXLEVEL
-#     # cd $parent_dir/max_level_$MAXLEVEL-coupled_$COUPLED/code
-    
-#     echo GAMMA = $GAMMA
-#     cd $parent_dir/gamma_$GAMMA/code
-
-#     ./run_simulation.sh $code_name 4
-
-# done
-
-for XMIN in 0 1 2 3 4
+for MAXLEVEL in 8 9 10 11 12 13
 do
-    for YMIN in 0 1 2 3
-    do
-        echo XMIN = $XMIN, YMIN = $YMIN
-        
-        cd $parent_dir/x_min_$XMIN-y_min_$YMIN/code
+    echo Max level = $MAXLEVEL
+    cd $parent_dir/max_level_$MAXLEVEL/code
 
-        ./run_simulation.sh $code_name 4
-
-    done
-    
+    ./run_simulation.sh $code_name 8
 done
+
+# for XMIN in 0 1 2 3 4
+# do
+#     for YMIN in 0 1 2 3
+#     do
+#         echo XMIN = $XMIN, YMIN = $YMIN
+        
+#         cd $parent_dir/x_min_$XMIN-y_min_$YMIN/code
+
+#         ./run_simulation.sh $code_name 4
+
+#     done
+    
+# done
