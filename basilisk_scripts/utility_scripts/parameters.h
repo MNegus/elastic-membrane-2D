037195fc5b9eca5ecd8e77c4e6612a6f6fa833e4
Michael <michaelnegus1@gmail.com>
2021-07-13 10:21:41 +0100

    More standard values

--- basilisk_scripts/utility_scripts/parameters.h
+++ basilisk_scripts/utility_scripts/parameters.h
@@ -16,7 +16,7 @@ const double DROP_RADIUS = 1.0; // Radius of droplet
 const double INITIAL_DROP_HEIGHT = 0.125; // Initial gap between drop and plate
 
 /* Membrane parameters */
-const double ALPHA = 2.0;
+const double ALPHA = 2;
 const double BETA = 1; // Tension term
 const double GAMMA = 2; // Bending term
 const double DELTA_T = 1e-4; // Timestep for membrane solution
@@ -24,15 +24,15 @@ const int COUPLED = 1; // Set to 1 if coupled
 
 /* Computational constants */
 const int MINLEVEL = 5; // Minimum refinement level 
-const int MAXLEVEL = 9; // Maximum refinement level
+const int MAXLEVEL = 12; // Maximum refinement level
 const double BOX_WIDTH = 6.0; // Width of the computational box
 const double MEMBRANE_RADIUS = 4.0; // Width of the membrane
 const double GFS_OUTPUT_TIMESTEP = 1e-2; // Time between gfs outputs
 const double PLATE_OUTPUT_TIMESTEP = 1e-4; // Time between plate outputs
 const double LOG_OUTPUT_TIMESTEP = 1e-4; // Time between log outputs
-const double MAX_TIME = 0.05; // Hard maximum time 
+const double MAX_TIME = 0.4; // Hard maximum time 
 const double MEMBRANE_START_TIME = 1e-3; // Time which to start membrane motion
-const int MOVIES = 0; // Set to 1 if making movies
+const int MOVIES = 1; // Set to 1 if making movies
 const int CUTOFF = 1; // Set to 1 to cutoff the pressure
 const int x_min_height = 2; // Min height to cutoff in x
 const int y_min_height = 3; // Min height to cutoff in y
