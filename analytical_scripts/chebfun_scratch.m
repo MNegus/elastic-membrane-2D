ee8fa5877524a94878d4f2c69316a0623170325b
Michael <michaelnegus1@gmail.com>
2021-07-22 14:28:13 +0100

    Removed redundant files

--- analytical_scripts/chebfun_scratch.m
+++
@@ -1,4 +0,0 @@
-addpath("~/chebfun");
-
-  g = chebfun(@(t) besselj(0,t),[0,100]);
-  plot(g), ylim([-.5 1])
--- analytical_scripts/pressures/etas_definition.m
+++
@@ -1,7 +0,0 @@
-function [etas, xs_etas] = etas_definition(xs, d, J, epsilon)
-
-    
-    % xs in the etas coordinates
-    
-
-end
