8613ca1e36ef2ff8a296708c4c2f92e661c89680
MNegus <michaelnegus1@gmail.com>
2021-07-29 16:09:21 +0100

    Equivalent form for C

--- analytical_scripts/time_dependent_quantities.m
+++ analytical_scripts/time_dependent_quantities.m
@@ -12,9 +12,8 @@ function [A, C, J] = time_dependent_quantities(d, d_t, w_t_fun, w_tt_fun, m_t_fu
 
         A = d * d_t * (1 - A_integral_1) - A_integral_2;
 
-
         %% Determine C(t)
-        C = A + A_integral_2;
+        C = d * d_t * (1 - A_integral_1) ;
 
         %% Determine J(t)
         B_integrand = @(s) (d * m_t_fun(d) - s .* m_t_fun(s)) ./ (d^2 - s.^2).^(3/2);
