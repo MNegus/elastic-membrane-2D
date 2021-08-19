89c5656d5d7ae688ee841369e98fd107c754b23e
Michael <michaelnegus1@gmail.com>
2021-08-19 19:17:21 +0100

    Classic removal, but less frequent

--- basilisk_scripts/utility_scripts/parameters.h
+++ basilisk_scripts/utility_scripts/parameters.h
@@ -20,18 +20,18 @@ const double ALPHA = 2;
 const double BETA = 1; // Tension term
 const double GAMMA = 0; // Bending term
 const double DELTA_T = 1e-4; // Timestep for membrane solution
-const int COUPLED = 0; // Set to 1 if membrane motion is coupled to the fluid
-const int STATIONARY = 1; // Set to 1 if enforcing membrane to be stationary
+const int COUPLED = 1; // Set to 1 if membrane motion is coupled to the fluid
+const int STATIONARY = 0; // Set to 1 if enforcing membrane to be stationary
 
 /* Computational constants */
 const int MINLEVEL = 5; // Minimum refinement level 
 const int MAXLEVEL = 13; // Maximum refinement level
 const double BOX_WIDTH = 6.0; // Width of the computational box
 const double MEMBRANE_RADIUS = 2.0; // Width of the membrane
-const double GFS_OUTPUT_TIMESTEP = 1e-3; // Time between gfs outputs
+const double GFS_OUTPUT_TIMESTEP = 1e-2; // Time between gfs outputs
 const double PLATE_OUTPUT_TIMESTEP = 1e-4; // Time between plate outputs
 const double LOG_OUTPUT_TIMESTEP = 1e-4; // Time between log outputs
-const double MAX_TIME = 0.3; // Hard maximum time 
+const double MAX_TIME = 0.4; // Hard maximum time 
 const double MEMBRANE_START_TIME = 1e-3; // Time which to start membrane motion
 const int MOVIES = 1; // Set to 1 if making movies
 const int CUTOFF = 1; // Set to 1 to cutoff the pressure
