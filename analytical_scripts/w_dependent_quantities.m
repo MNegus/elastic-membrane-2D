2518bcbb5401e29c46c90a8bdda13bff35dc6ccb
Michael <michaelnegus1@gmail.com>
2021-08-24 11:22:38 +0100

    Function to determine pressure, turnover point and jet thickness given w

---
+++ analytical_scripts/w_dependent_quantities.m
@@ -0,0 +1,51 @@
+function [p, d, d_t, J] = w_dependent_quantities(xs, t, w_t, w_tt, ...
+    w_fun, w_x_fun, d_previous, d_t_previous, pressure_type, EPSILON, DELTA_T)
+    
+    %% Determine continous functions for w_t_vals and w_tt_vals
+    tic
+    w_t_fun = @(x) interp1(xs, w_t, x, 'linear', 'extrap');
+    w_tt_fun = @(x) interp1(xs, w_tt, x, 'linear', 'extrap');
+    toc
+
+    %% Determine d and d_t
+    tic
+    [d, d_t] = turnover_point_trapz(xs, t, d_previous, d_t_previous, w_fun, w_t_fun, w_x_fun, EPSILON, DELTA_T);
+    toc
+
+    % Finds d_idx such that x(d_idx) < epsilon * d but x(d_idx) >= epsilon * d
+    d_idx = sum(xs < EPSILON * d);
+
+    %% Determine m and its derivatives
+    tic
+    if (d_idx < 2)
+        m_t_fun = @(s) zeros(size(s));
+        m_tt_fun = @(s) zeros(size(s));
+    else
+        s_vals = xs(1 : d_idx) / EPSILON;
+
+        m_t = cumtrapz(s_vals, w_t(1 : d_idx), 1);
+        m_tt = cumtrapz(s_vals, w_tt(1 : d_idx), 1);
+
+        m_t_fun = @(s) interp1(s_vals, m_t, s, 'linear', 'extrap');
+        m_tt_fun = @(s) interp1(s_vals, m_tt, s, 'linear', 'extrap');
+    end
+    toc
+    %% Determine time-dependent quantities
+    tic
+    [A, C, J] = time_dependent_quantities(d, d_t, w_t_fun, w_tt_fun, m_t_fun, EPSILON);
+    toc
+    %% Determine pressure at current timestep
+    tic
+    if pressure_type == "outer"
+        p = outer_pressure(xs, m_tt_fun, w_tt_fun, d, A, EPSILON);
+    elseif pressure_type == "composite"
+        p = composite_pressure(xs, t, d, d_t, A, C, J, m_tt_fun, w_tt_fun, EPSILON);
+    else
+        error("Invalid pressure_type");
+    end
+
+    % Restrict large values of p
+    p(p > 1e4) = 0;
+    toc
+
+end
