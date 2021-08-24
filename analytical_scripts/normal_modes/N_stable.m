c36ac66eb75f43b5e4e1e9311b5443e3de4de1b5
Michael <michaelnegus1@gmail.com>
2021-08-24 11:16:51 +0100

    Makes valid for all values of gamma

--- analytical_scripts/normal_modes/N_stable.m
+++ analytical_scripts/normal_modes/N_stable.m
@@ -1,10 +1,19 @@
 function N = N_stable(alpha, beta, gamma, L, q, delta_d)
 
-    lambda = @(n) pi * (2 * n - 1) / (2 * L);
+%     lambda = @(n) pi * (2 * n - 1) / (2 * L);
+% 
+%     tN = @(N) 2 * pi * sqrt(alpha / (beta * lambda(N)^2 + gamma * lambda(N)^4));
+%     
+%     N_zero_fun = @(N) delta_d - tN(N) / q;
+%     
+%     N = fsolve(N_zero_fun, 2);
 
-    tN = @(N) 2 * pi * sqrt(alpha / (beta * lambda(N)^2 + gamma * lambda(N)^4));
-    
-    N_zero_fun = @(N) delta_d - tN(N) / q;
-    
-    N = fsolve(N_zero_fun, 2);
+
+    if (gamma > 0)
+        c = alpha * (2 * pi / (q * delta_d))^2;
+        lambda2 = (-beta + sqrt(beta^2 + 4 * gamma * c)) / (2 * gamma);
+        N = 0.5 + (L / pi) * sqrt(lambda2);
+    else
+        N = 0.5 + (2 * L / pi) * sqrt(alpha / beta);
+    end
 end
