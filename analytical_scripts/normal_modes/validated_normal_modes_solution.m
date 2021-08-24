4bb256443b9f8c357ab409d1bde28d143bb8a3f8
Michael <michaelnegus1@gmail.com>
2021-08-24 11:17:36 +0100

    Version to run to ensure N and delta t are appropriate

--- analytical_scripts/normal_modes/validated_normal_modes_solution.m
+++ analytical_scripts/normal_modes/validated_normal_modes_solution.m
@@ -22,10 +22,11 @@ function [N, delta_d, ds, as, a_ts, a_tts, q_ts] ...
     converged = 0;
     while (converged == 0)
         %% Determines N_max for current delta_d
-        N_max = N_stable(alpha, beta, gamma, L, q, delta_d);
+        delta_d
+        N_max = N_stable(alpha, beta, gamma, L, q, delta_d)
         
         %% Initialise N and solves ode
-        N0 = min(32, floor(N_max / 2));
+        N0 = min(floor(N_max / 4));
         N = N0;
         [t_vals_d_form, d_vals_d_form, as_d_form, a_ts_d_form, kvals] ...
             = a_ode_solution(alpha, beta, gamma, epsilon, delta_d, d_max, N, L);
@@ -36,9 +37,9 @@ function [N, delta_d, ds, as, a_ts, a_tts, q_ts] ...
         
         %% Loops until we've found an N or N >= N_max
         diff = 1e6;
-        while ((diff > tol) || (N >= N_max))
+        while ((diff > tol) && (2 * N < N_max))
             % Doubles N
-            new_N = 2 * N;
+            new_N = 2 * N
             
             % Solves ode with new N
             [new_t_vals_d_form, new_d_vals_d_form, new_as_d_form, new_a_ts_d_form, new_kvals] ...
@@ -51,11 +52,24 @@ function [N, delta_d, ds, as, a_ts, a_tts, q_ts] ...
                     new_kvals, alpha, delta_t);
             
             % Compares ws solution for all time between the two
-            diff = 1e6;
+            diff = 0;
+            figure(1);
             for k = 1 : length(ts)
-                [ws, ~, ~] = w_solution_normal_modes(xs, as, a_ts, q_ts, L, N);
-                [new_ws, ~, ~] = w_solution_normal_modes(xs, new_as, new_a_ts, new_q_ts, L, new_N);
-                diff = min(diff, max(abs(ws - new_ws)))
+                [ws, ~, ~] = w_solution_normal_modes(xs, as(k, :), a_ts(k, :), q_ts(k, :), L, N);
+                [new_ws, ~, ~] = w_solution_normal_modes(xs, new_as(k, :), new_a_ts(k, :), new_q_ts(k, :), L, new_N);
+                diff = max(diff, max(abs(ws - new_ws)))
+                delta_d
+                N
+                if (diff > tol)
+                    break;
+                end
+                plot(xs, ws);
+                hold on;
+                plot(xs, new_ws);
+                hold off;
+                title(k);
+                drawnow;
+                pause(0.0000001);
             end
             
             % Update solutions
@@ -65,10 +79,14 @@ function [N, delta_d, ds, as, a_ts, a_tts, q_ts] ...
             a_ts = new_a_ts;
             a_tts = new_a_tts;
             q_ts = new_q_ts;
+            
+            if (diff < tol)
+                disp("HELLO???");
+            end
         end
        
         %% Checks for value of N
-        if N >= N_max
+        if 2 * N >= N_max
            N = N0;
            delta_d = delta_d / 2;
         else
