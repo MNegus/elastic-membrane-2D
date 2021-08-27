255b30956e980c75c3da92bd609b0c4ba95e946a
Michael <michaelnegus1@gmail.com>
2021-08-27 23:18:08 +0100

    Beta varying run

--- basilisk_scripts/utility_scripts/loop_run.sh
+++ basilisk_scripts/utility_scripts/loop_run.sh
@@ -4,18 +4,32 @@ parent_dir=$1
 code_name=$2
 move_dest=$3
 
-for GAMMA in 0.1 0.2 0.4 0.8 1.6 3.2 6.4 12.8
+ALPHA=1;
+
+for GAMMA in 0.4 0.01
 do
-    echo GAMMA = $GAMMA
+    for BETA in 0.1 0.2 0.4 0.8 1.6 3.2 6.4 12.8
+    do 
+        echo BETA = $BETA, GAMMA = $GAMMA
+        
+        cd $parent_dir/alpha_$ALPHA-beta_$BETA-gamma_$GAMMA/code
 
-    cd $parent_dir/gamma_$GAMMA/code
-
-    ./run_simulation.sh $code_name 4
-
-    cd $parent_dir
-
-    mv gamma_$GAMMA $move_dest
+        ./run_simulation.sh $code_name 8
+    done
 done
+
+# for GAMMA in 0.1 0.2 0.4 0.8 1.6 3.2 6.4 12.8
+# do
+#     echo GAMMA = $GAMMA
+
+#     cd $parent_dir/gamma_$GAMMA/code
+
+#     ./run_simulation.sh $code_name 4
+
+#     cd $parent_dir
+
+#     mv gamma_$GAMMA $move_dest
+# done
 
 # for COARSEN in 0 1 2 3
 # do
