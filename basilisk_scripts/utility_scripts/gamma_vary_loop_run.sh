0ac4cbd43f71c16a0ae8a481a8a3b88f7e2b804f
Michael <michaelnegus1@gmail.com>
2021-08-30 21:38:09 +0100

    Loop copy/run files

---
+++ basilisk_scripts/utility_scripts/gamma_vary_loop_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for GAMMA in 0.8 1.6 3.2 6.4 12.8
+do
+    for BETA in 0
+    do 
+        echo BETA = $BETA, GAMMA = $GAMMA
+        
+        cd $parent_dir/alpha_$ALPHA-beta_$BETA-gamma_$GAMMA/code
+
+        ./run_simulation.sh $code_name 8
+    done
+done
