1fb592ce00078947e3a0dc694d69f5b6974b7606
Michael <michaelnegus1@gmail.com>
2021-09-20 09:26:05 +0100

    Update stationary pressure solutions

--- analytical_scripts/pressures/composite_pressure_stationary.m
+++ analytical_scripts/pressures/composite_pressure_stationary.m
@@ -1,10 +1,17 @@
-function ps = composite_pressure_stationary(xs, t, d, d_t, A, C, J, epsilon)
+function ps = composite_pressure_stationary(xs, t, epsilon)
 
     if (t == 0)
         ps = zeros(size(xs));
     else
+        % Calculate time dependent terms
+        d = 2 * sqrt(t);
+        d_t = 1 / sqrt(t);
+        A = d * d_t;
+        C = A;
+        J = pi * d / (8 * d_t^2);
+        
         % Calculate outer pressure
-        outer_ps = outer_pressure_stationary(xs, d, A, epsilon);
+        outer_ps = outer_pressure_stationary(xs, t, epsilon);
 
         % Calculate overlap pressure
         overlap_ps = overlap_pressure(xs, d, C, epsilon);
--- analytical_scripts/pressures/outer_pressure_stationary.m
+++ analytical_scripts/pressures/outer_pressure_stationary.m
@@ -1,12 +1,22 @@
- function ps = outer_pressure_stationary(xs, d, A, epsilon)
+ function ps = outer_pressure_stationary(xs, t, epsilon)
     ps = zeros(size(xs));
     
     xhats = xs / epsilon;
-
-    %% Finds idx such that xhat = d
-    idx = sum(xhats < d);
     
-    %% Returns p values
-    ps(1 : idx) = A ./ sqrt(epsilon^2 * d^2 - xs(1 : idx).^2);
+    if (t > 0)
+        %% Determine d and A
+        d = 2 * sqrt(t);
+        d_t = 1 / sqrt(t);
+        A = d * d_t;
+
+        %% Finds idx such that xhat = d
+        idx = sum(xhats < d);
+
+        %% Returns p values
+        ps(1 : idx) = A ./ sqrt(epsilon^2 * d^2 - xs(1 : idx).^2);
+        
+    end
+    
+    
 
 end
