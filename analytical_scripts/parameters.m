2e7c70af4963e948353a5b1e48d32b40bb2c6497
Michael <michaelnegus1@gmail.com>
2021-09-20 09:27:53 +0100

    File for saving the parameters

---
+++ analytical_scripts/parameters.m
@@ -0,0 +1,18 @@
+function [EPSILON, ALPHAS, BETAS, GAMMAS, L, T_MAX, DELTA_T, N_MEMBRANE] ...
+    = parameters()
+
+    %% Define parameters
+    EPSILON = 1;
+    ALPHAS = 1 / EPSILON^2; 
+    BETAS =  0 * EPSILON^2; 
+%     GAMMAS = [0.1, 0.2, 0.4, 0.8, 1.6, 3.2, 6.4, 12.8] * EPSILON;
+    GAMMAS = [0.1, 0.4, 1.6, 6.4] * EPSILON;
+    L = 16;
+    T_MAX = 0.4;
+    DELTA_T = 1e-4;
+
+    % FD parameters
+    N_MEMBRANE = 10924;
+    
+
+end
