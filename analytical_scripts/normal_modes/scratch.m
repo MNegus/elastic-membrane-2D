29a945e46ff0defee4e50a430fc31ab4f6a96216
MNegus <michaelnegus1@gmail.com>
2021-09-30 17:36:14 +0100

    Example of prescribed pressure used later for validation

--- analytical_scripts/normal_modes/scratch.m
+++ analytical_scripts/normal_modes/scratch.m
@@ -1,17 +1,46 @@
-alpha = 0.1;
-beta = 0;
-gamma = 1e-4;
+alpha = 2;
+beta = 1;
+gamma = 2;
 epsilon = 1;
-L = 4;
-q = 10;
-delta_t = 1e-4;
+L = 16;
+N = 128;
+
+lambda = @(n) pi * (2 * n - 1) / (2 * L);
+as = zeros(N, 1);
+
+xs = linspace(0, L, 1024);
+dt = 1e-2;
 tmax = 0.25;
-% N_max = N_stable(alpha, beta, gamma, L, q, delta_d)
+tvals = 0 : dt : tmax
 
-N_MEMBRANE = 1024;
-DELTA_X = L / (N_MEMBRANE - 1); 
-xs = (0 : DELTA_X : L - DELTA_X)';
 
-%%
-[N, delta_d, as, a_ts, a_tts, q_ts] ...
-    = validated_normal_modes_solution(alpha, beta, gamma, epsilon, N, L, tmax, delta_t, xs)
+close(figure(1));
+figure(1);
+for q = 1 : length(tvals)
+   t = tvals(q)
+   
+   for n = 1 : N
+        k = beta * lambda(n)^2 + gamma * lambda(n)^4;
+        as(n) = 10 * (-2 * alpha + k * t^2 + 2 * alpha * cos(sqrt(k / alpha) * t)) / (k^2 * sqrt(n));
+   end
+   
+   plot(xs, w_solution(xs, as, L, N));
+   drawnow;
+   pause(0.1);
+    
+end
+
+
+function ws = w_solution(xs, as, L, N)
+    ws = zeros(size(xs));
+    
+    lambda = @(n) pi * (2 * n - 1) / (2 * L);
+    
+    
+    %% FIND AN ALTERNATIVE USING MATRIX MULTIPLICATION
+    for n = 1 : N
+        ws = ws + as(n) * cos(lambda(n) * xs) / sqrt(L);
+    end
+
+end
+
