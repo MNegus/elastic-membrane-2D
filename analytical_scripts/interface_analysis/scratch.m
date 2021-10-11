a9da9088fbe61564bf6116bdd5fc67a004d30969
MNegus <michaelnegus1@gmail.com>
2021-10-11 17:01:32 +0100

    Plots velocity too, with good agreement

--- analytical_scripts/interface_analysis/scratch.m
+++ analytical_scripts/interface_analysis/scratch.m
@@ -6,10 +6,12 @@ IMPACT_TIME = 0.125;
 
 % Selects level
 level = 11;
-data = readmatrix(sprintf("turnover_points_basilisk_level_%d.txt", level));
+% data = readmatrix(sprintf("turnover_points_basilisk_level_%d.txt", level));
+data = readmatrix("turnover_points_ignore_y.txt");
 ts = data(:, 1) - IMPACT_TIME;
 ds = data(:, 2);
 Js = data(:, 3);
+d_ts = data(:, 4);
 fluxes = data(:, 6);
 energies = data(:, 7);
 
@@ -17,7 +19,7 @@ energies = data(:, 7);
 analytical_ts = 0 : DELTA_T : max(ts);
 analytical_ds = 2 * sqrt(analytical_ts);
 analytical_d_ts = 1 ./ sqrt(analytical_ts);
-analytical_Js = pi * analytical_ds ./ ( 8 * analytical_d_ts.^2);
+analytical_Js = pi * analytical_ds ./ ( 8 * analytical_d_ts.^2) * (1 + 4 / pi);
 analytical_fluxes = pi * ones(size(analytical_ts));
 analytical_energy = pi * analytical_ts;
 
@@ -43,9 +45,21 @@ xlabel("t");
 ylabel("J(t)");
 title("Height of turnover point");
 
-%% Plot energy flux
+%% Plot turnover point velocity
 close(figure(3));
 figure(3);
+hold on;
+plot(analytical_ts, analytical_d_ts, 'linewidth', 5, 'color', 0.5 * [1 1 1]);
+plot(ts, d_ts, 'linewidth', 2);
+legend(["Analytical", "Basilisk"]);
+xlabel("t");
+ylabel("d'(t)");
+title("Turnover point velocity");
+
+
+%% Plot energy flux
+close(figure(6));
+figure(6);
 hold on;
 plot(analytical_ts, analytical_fluxes, 'linewidth', 5, 'color', 0.5 * [1 1 1]);
 plot(ts, fluxes, 'linewidth', 2);
@@ -55,8 +69,8 @@ ylabel("Flux");
 title("Flux into jet");
 
 %% Plot energy into jet
-close(figure(4));
-figure(4);
+close(figure(7));
+figure(7);
 hold on;
 plot(analytical_ts, analytical_energy, 'linewidth', 5, 'color', 0.5 * [1 1 1]);
 plot(ts, energies, 'linewidth', 2);
