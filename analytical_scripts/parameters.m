fe571e73674d85d2528ff14c8e628561940e7b8a
MNegus <michaelnegus1@gmail.com>
2021-11-14 11:15:11 +0000

    Adding more values to the parameters file

--- analytical_scripts/parameters.m
+++ analytical_scripts/parameters.m
@@ -1,19 +1,43 @@
 function [EPSILON, ALPHAS, BETAS, GAMMAS, L, T_MAX, DELTA_T, N_MEMBRANE, IMPACT_TIME] ...
     = parameters()
 
+    %% Define dimensional values (in SI units)
+%     rho_m = 1200; % Membrane density
+%     nu = 10^-3; % Membrane thickness
+%     E = 2.2 * 10^9; % Membrane Young's modulus
+%     T = 17; % Membrane tension
+%     rho_l = 789; % Droplet density
+%     R = 1e-3; % Droplet radius
+%     V = 1; % Droplet speed
+    
+
     %% Define parameters
     EPSILON = 1;
-    ALPHAS = [1]; 
-%     ALPHAS = [0.5, 0.25, 0.125];
-    BETAS =  0 * EPSILON^2; 
-    GAMMAS = [1.6, 3.2, 6.4] * EPSILON;
-%     GAMMAS = 1;
+%     ALPHAS = rho_m * nu / (EPSILON^2 * rho_l * R)
+%     BETAS =  EPSILON^2 * T / (rho_l * R * V)
+%     GAMMAS = EPSILON^2 * E * nu^3 / (3 * rho_l * R^3 * V^2)
+
+    % alpha_varying
+    ALPHAS = [1, 2, 4, 8] / EPSILON^2;
+    BETAS = zeros(size(ALPHAS)) * EPSILON^2;
+    GAMMAS = 2 * (EPSILON^2 * ALPHAS).^3 * EPSILON^2;
+
+    % beta_varying
+    BETAS = [0, 10, 20, 40, 80, 160, 320, 640, 1280] * EPSILON^2;
+    ALPHAS = ones(size(BETAS)) / EPSILON^2;
+    GAMMAS = 2 * (EPSILON^2 * ALPHAS).^3 * EPSILON^2;
+    
+    % gamma_varying
+    GAMMAS = [2, 4, 8, 16, 32, 64, 128] * EPSILON^2;
+    ALPHAS = 2 * ones(size(GAMMAS)) / EPSILON^2;
+    BETAS = zeros(size(GAMMAS)) * EPSILON^2;
+    
     L = 16;
     T_MAX = 0.4;
     DELTA_T = 1e-4;
 
     % FD parameters
-    N_MEMBRANE = 10923;
+    N_MEMBRANE = 10924;
     
     IMPACT_TIME = 0.125;
 end
