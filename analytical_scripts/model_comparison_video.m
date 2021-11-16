5a06e203d5b121df7b50ce3034e21de338cdcf66
Michael <michaelnegus1@gmail.com>
2021-11-16 03:39:12 +0000

    Made dimensional

--- analytical_scripts/model_comparison_video.m
+++ analytical_scripts/model_comparison_video.m
@@ -16,7 +16,7 @@ V = 5;
 R = 1e-3;
 
 millimetre_scale = R / 1e-3;
-millisecond_scale = (R / V) / 1e3;
+millisecond_scale = (R / V) / 1e-3;
 
 %% Time variables
 IMPACT_TIME = 0.125;
@@ -43,17 +43,16 @@ nm_dir = sprintf("%s/normal_modes", analytical_parameter_dir);
 dns_master_dir = "/media/michael/newarre/elastic_membrane/basilisk_parameter_sweeping/tension_varying";
 dns_parameter_dir = sprintf("%s/alpha_%g-beta_%d-gamma_%g/raw_data", dns_master_dir, ALPHA, BETA, GAMMA);
 
-stationary_dns_dir = "/media/michael/newarre/elastic_membrane/confirmation_data/stationary_benchmark";
+stationary_dns_dir = "/media/michael/newarre/elastic_membrane/stationary_membrane/raw_data";
+
+
 
 %% Load in DNS turnover point data
 turnover_mat = readmatrix(sprintf("%s/turnover_points_basilisk.txt", dns_parameter_dir));
 ds_dns = turnover_mat(:, 2);
 
-turnover_mat = readmatrix(sprintf("%s/turnover_points.txt", stationary_dns_dir));
-ds_stationary_dns = zeros(NO_TIMESTEPS, 1);
-ds_stationary_dns(turnover_mat(:, 1)) = turnover_mat(:, 2);
-
-
+turnover_mat = readmatrix(sprintf("%s/turnover_points_basilisk.txt", stationary_dns_dir));
+ds_stationary_dns = turnover_mat(:, 2);
 
 %% Load in normal modes solution
 N_mat = matfile(sprintf("%s/N.mat", nm_dir));
@@ -77,27 +76,27 @@ fontsize = 20;
 % Figure 1 setting up
 close(figure(1));
 fig1 = figure(1);
+layout = tiledlayout(3, 1);
+layout.Padding = 'compact';
+
+nexttile(1, [2, 1]);
 stationary_line = animatedline('color', [255, 73, 73] / 255, 'linewidth', 4);
 moving_line = animatedline('color', [0 0 0], 'linewidth', 4);
-
 upper_d_line = animatedline('color', [0 0 0], 'linewidth', 2, 'linestyle', '--');
 
 xlim([0, 1.25]);
 ylim([0, 0.5]);
 grid on;
 set(gca, 'TickLabelInterpreter', 'latex', 'Fontsize', fontsize);
-%     xticklabels(ax1,{});
-x0=400;
-y0=400;
-width=1800;
-height=500;
-set(gcf,'position',[x0,y0,width,height]);
-xlabel("$x^*$ (mm)", "Fontsize", fontsize, "Interpreter", "latex");
+xticks([0 : 0.1 : 1.2]);
+set(gca,'Xticklabel',[]) 
+% xlabel("$x^*$ (mm)", "Fontsize", fontsize, "Interpreter", "latex");
 ylabel("$z^*$ (mm)", "Fontsize", fontsize, "Interpreter", "latex");
+legend(["DNS: Rigid membrane", "DNS: Deformable membrane", "Turnover point"], ...
+    'Location', 'Northwest', "Interpreter", "latex")
 
 % Figure 2 setting up
-close(figure(2));
-fig2 = figure(2);
+nexttile(3, [1, 1]);
 ws_dns_line = animatedline('color', [0 0 0], 'linewidth', 4);
 ws_fd_line = animatedline('color', 0.5 * [1 1 1], 'linestyle', '--', 'linewidth', 4);
 ws_nm_line = animatedline('color', 0.5 * [1 1 1], 'linestyle', ':', 'linewidth', 4);
@@ -106,16 +105,21 @@ lower_d_line = animatedline('color', [0 0 0], 'linewidth', 2, 'linestyle', '--')
 grid on;
 set(gca, 'TickLabelInterpreter', 'latex', 'Fontsize', fontsize);
 xlim([0, 1.25]);
-x0=400;
-y0=100;
-width=1800;
-height=250;
-set(gcf,'position',[x0,y0,width,height])
 xlabel("$x^*$ (mm)", "Fontsize", fontsize, "Interpreter", "latex");
+xticks([0 : 0.1 : 1.2]);
 ylabel("$-w^*(x^*, t^*)$ (mm)", "Fontsize", fontsize, "Interpreter", "latex");
 ax = gca;
 ax.YAxis.Exponent = 0;
-ytickformat(gca, '%.4f');
+ytickformat(gca, '%.5f');
+legend([ws_dns_line, ws_fd_line, ws_nm_line], ["DNS", "Analytical: Finite differences", "Analytical: Normal modes"], ...
+    'Location', 'southeast', "Interpreter", "latex")
+
+
+x0=400;
+y0=100;
+width=1800;
+height=801;
+set(gcf,'position',[x0,y0,width,height])
 
 %% Video writers set up
 interface_obj = VideoWriter('interfaces.avi');
@@ -124,15 +128,16 @@ membrane_obj = VideoWriter('membrane.avi');
 open(membrane_obj);
 
 %% Loop over time
-for k = 1000 : 100 : length(T_VALS)
+for k = 1100 : 10 : length(T_VALS)
     % Updates time
     t = T_VALS(k)
     
     %% DNS interface plotting
-    figure(1);
+    nexttile(1, [2, 1]);
+    title(sprintf("$t^*$ = %.4f (ms)", t * millisecond_scale), "Interpreter", "latex");
     
     % Adds interfacial lines
-    stationary_filename = sprintf("%s/interfaces/interface_%d.txt", stationary_dns_dir, k);
+    stationary_filename = sprintf("%s/interface_%d.txt", stationary_dns_dir, k);
     moving_filename = sprintf("%s/interface_%d.txt", dns_parameter_dir, k);
     for filename = [moving_filename, stationary_filename, ]
         [start_points, end_points] = read_interface_points(filename, true); 
@@ -142,14 +147,28 @@ for k = 1000 : 100 : length(T_VALS)
         [~, sorted_idxs] = sort(uniq_points(:, 1));
         sorted_points = uniq_points(sorted_idxs, :);
         x_interp = @(z) interp1(sorted_points(:, 1), sorted_points(:, 2), z);
-        zs = linspace(0, 2, 1e5);
+        
+        
+        if (k <= 1250)
+            x_vals = zeros(1e4, 1);
+            z_vals = zeros(1e4, 1);
+            z_vals(2 : end) = linspace(min(sorted_points(:, 1)), max(sorted_points(:, 1)), 1e4 - 1);
+            x_vals(2 : end) = x_interp(z_vals(2 : end));
+        
+            z_vals(1) = min(z_vals(2:end));
+        else
+            z_vals = linspace(min(sorted_points(:, 1)), max(sorted_points(:, 1)), 1e4);
+            x_vals = x_interp(z_vals);
+        end
+        
+        
         
         if (filename == stationary_filename)
             clearpoints(stationary_line);
-            addpoints(stationary_line, x_interp(zs), zs);
+            addpoints(stationary_line, x_vals, z_vals);
         else
             clearpoints(moving_line);
-            addpoints(moving_line, x_interp(zs), zs);
+            addpoints(moving_line, x_vals, z_vals);
         end
     end
     hold off;
@@ -159,11 +178,10 @@ for k = 1000 : 100 : length(T_VALS)
         addpoints(upper_d_line, ds_dns(k) * [1 1], [0 1]);
     end
     
-    f1 = getframe(fig1);
-    writeVideo(interface_obj, f1);
+    
     
     %% Membrane plotting
-    figure(2);
+    nexttile(3, [1, 1]);
     dns_ws_mat = readmatrix(sprintf("%s/w_%d.txt", dns_parameter_dir, k - 1));
     unsorted_xs_dns = dns_ws_mat(:, 1);
     [xs_dns, sort_idxs] = sort(unsorted_xs_dns);
@@ -210,17 +228,18 @@ for k = 1000 : 100 : length(T_VALS)
     end
     
     if (t <= 0)
-        ylim([-1.1 * max(ws_dns), 0]);
+        ylim([min(-1.1 * max(ws_dns), -1.5e-4), max(0, -1.1 * min(ws_dns))]);
     else
         ylim([-1.1 * max([max(ws_nm), max(ws_composite), max(ws_dns)]), max([0, -1.5 * min(ws_nm)])]);
     end
 
-    f2 = getframe(fig2);
-    writeVideo(membrane_obj, f2);
     
     drawnow;
+    
+    f1 = getframe(fig1);
+    writeVideo(interface_obj, f1);
+    
     pause(0.01);
 end
 close(interface_obj);
-close(membrane_obj);
 
