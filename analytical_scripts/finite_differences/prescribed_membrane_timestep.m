79e0cb6ebfc08e04d4a747cede1fc6942b934b93
Michael <michaelnegus1@gmail.com>
2021-12-21 10:45:06 +0000

    New prescribed coefficient

--- analytical_scripts/finite_differences/prescribed_membrane_timestep.m
+++ analytical_scripts/finite_differences/prescribed_membrane_timestep.m
@@ -11,18 +11,18 @@ function [w_next, p, w_t, d, d_t, J] = prescribed_membrane_timestep(...
     w_t = (w_next - w_previous) / (2 * DELTA_T);
     w_tt = (w_previous - 2 * w + w_next) / (DELTA_T^2);
     
-    w_x = zeros(M, 1);
-    w_x(2 : M - 1) = (w(3 : M) - w(1 : M - 2)) / (2 * DELTA_X);
-    w_x(M) = -w(M - 1) / (2 * DELTA_X);
-    w_x_fun = @(x) interp1(xs, w_x, x, 'linear', 'extrap');
-    
-    % Continous functions for w and its derivatives
-    w_fun = @(x) interp1(xs, w, x, 'linear', 'extrap');
-    w_t_fun = @(x) interp1(xs, w_t, x, 'linear', 'extrap');
-    w_tt_fun = @(x) interp1(xs, w_tt, x, 'linear', 'extrap');
+%     w_x = zeros(M, 1);
+%     w_x(2 : M - 1) = (w(3 : M) - w(1 : M - 2)) / (2 * DELTA_X);
+%     w_x(M) = -w(M - 1) / (2 * DELTA_X);
+%     w_x_fun = @(x) interp1(xs, w_x, x, 'linear', 'extrap');
+%     
+%     % Continous functions for w and its derivatives
+%     w_fun = @(x) interp1(xs, w, x, 'linear', 'extrap');
+%     w_t_fun = @(x) interp1(xs, w_t, x, 'linear', 'extrap');
+%     w_tt_fun = @(x) interp1(xs, w_tt, x, 'linear', 'extrap');
     
     % Pressure, turnover points and jet thickness
-    p = prescribed_pressure(xs, ALPHA, L, Ne);
+    p = prescribed_pressure(xs, ALPHA, EPSILON, L, Ne);
     d = 2 * sqrt(t);
     d_t = 1 / sqrt(t);
     J = pi * d / (8 * d_t^2);
--- analytical_scripts/finite_differences/prescribed_pressure.m
+++ analytical_scripts/finite_differences/prescribed_pressure.m
@@ -1,6 +1,6 @@
-function ps = prescribed_pressure(xs, alpha, L, Ne)
+function ps = prescribed_pressure(xs, alpha, epsilon, L, Ne)
 
     lambdas = pi * (2 * (1 : Ne) - 1) / (2 * L);
-    p_ns = alpha ./ sqrt(L * lambdas);
+    p_ns = (alpha / epsilon^2) ./ sqrt(L * lambdas);
     ps = sum(p_ns .* cos(xs * lambdas), 2) / sqrt(L);
 end
