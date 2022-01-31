6f51e7922e8bd4dbccbfe51ceb1db44ead532f1f
Michael <michaelnegus1@gmail.com>
2022-01-31 10:25:38 +0000

    Utilities for test case

--- basilisk_scripts/utility_scripts/parameters.h
+++ basilisk_scripts/utility_scripts/parameters.h
@@ -25,10 +25,10 @@ const int COUPLED = 1; // Set to 1 if membrane motion is coupled to the fluid
 const int STATIONARY = 0; // Set to 1 if enforcing membrane to be stationary
 
 /* Computational constants */
-const int MINLEVEL = 7; // Minimum refinement level 
-const int MAXLEVEL = 13; // Maximum refinement level
-const double BOX_WIDTH = 24.0; // Width of the computational box
-const double MEMBRANE_RADIUS = 16.0; // Width of the membrane
+const int MINLEVEL = 5; // Minimum refinement level 
+const int MAXLEVEL = 12; // Maximum refinement level
+const double BOX_WIDTH = 6.0; // Width of the computational box
+const double MEMBRANE_RADIUS = 2; // Width of the membrane
 const double GFS_OUTPUT_TIMESTEP = 1e-2; // Time between gfs outputs
 const double PLATE_OUTPUT_TIMESTEP = 1e-4; // Time between plate outputs
 const double LOG_OUTPUT_TIMESTEP = 1e-4; // Time between log outputs
