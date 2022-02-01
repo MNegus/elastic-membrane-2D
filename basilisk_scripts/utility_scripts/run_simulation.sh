d7894378db094a62bff52edde5684fcda6e3d5df
MNegus <michaelnegus1@gmail.com>
2022-02-01 17:28:18 +0000

    Added reference to vofi

--- basilisk_scripts/utility_scripts/run_simulation.sh
+++ basilisk_scripts/utility_scripts/run_simulation.sh
@@ -10,13 +10,13 @@ mkdir ${basilisk_script}
 
 # Compiles basilisk_script.c using the desired flags with qcc
 rm ${basilisk_script}.o
-qcc -O2 -L$BASILISK/gl -lglutils -lfb_osmesa -lGLU -lOSMesa -lm -fopenmp -g -Wall -pipe -D_FORTIFY_SOURCE=2 -DDUMBGL -c ${basilisk_script}.c 
+qcc -O2 -L$BASILISK/gl -L$HOME/local/lib -lvofi -lglutils -lfb_osmesa -lGLU -lOSMesa -lm -fopenmp -g -Wall -pipe -D_FORTIFY_SOURCE=2 -DDUMBGL -c ${basilisk_script}.c 
 
 # Compiles membrane-equation.c using gcc
 gcc -O2 -c membrane-equation.c 
 
 # Links membrane_script.o and basilisk_script.o, using the same flags as before PLUS lapacke and lapack
-qcc membrane-equation.o ${basilisk_script}.o -O2 -L$BASILISK/gl -llapacke -llapack -lglutils -lfb_osmesa -lGLU -lOSMesa -lm -fopenmp -g -Wall -pipe -D_FORTIFY_SOURCE=2 -DDUMBGL -o ${basilisk_script}/${basilisk_script} -lm
+qcc membrane-equation.o ${basilisk_script}.o -O2 -L$BASILISK/gl -L$HOME/local/lib -lvofi -llapacke -llapack -lglutils -lfb_osmesa -lGLU -lOSMesa -lm -fopenmp -g -Wall -pipe -D_FORTIFY_SOURCE=2 -DDUMBGL -o ${basilisk_script}/${basilisk_script} -lm
 
 # Move into the basilisk_script directory
 cd ${basilisk_script}
