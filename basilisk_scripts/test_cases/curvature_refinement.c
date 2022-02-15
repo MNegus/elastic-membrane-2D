f7f43868d634626c10a9eca512ca691f751c04ec
MNegus <michaelnegus1@gmail.com>
2022-02-15 15:02:59 +0000

    Refinement study for the curvature by changing the maximum refinement

---
+++ basilisk_scripts/test_cases/curvature_refinement.c
@@ -0,0 +1,187 @@
+/* curvature_test.c
+    Tests the implementation of the moving frame curvature. We initialise a 
+    stationary droplet with a specific membrane displacement. We do not include
+    any hydrodynamics, and just check whether the curvature calculation of the
+    droplet matches the stationary case. 
+
+    Similar to the curvature.c test in the Basilisk source code, we use vofi to
+    initialise the droplet, and use the curvature statistics to check the output
+    matches the curvature. This time, we keep the droplet position and radius
+    the same, but still vary the refinement level and perhaps the membrane 
+    displacment. 
+
+*/
+
+#define MOVING 1
+
+#include <vofi.h>
+#include "parameters.h" // Includes all defined parameters
+
+
+scalar W[], Wx[], Wxx[];
+vector htest[];
+
+#include "fractions.h"
+#include "curvature.h"
+
+double mag = 0;
+int gfs_output_no = 1;
+double drop_centre;
+double DROP_REFINED_WIDTH = 0.01;
+
+
+
+
+double membrane_position(double x) {
+/* Continuous function for the membrane position */
+    if (x <= MEMBRANE_RADIUS) {
+        return mag * cos(pi * x / (2 * MEMBRANE_RADIUS));
+    } else {
+        return 0;
+    }
+}
+
+double membrane_first_derivative(double x) {
+/* Continuous function for the first derivative of the membrane position */
+    if (x <= MEMBRANE_RADIUS) {
+        return -mag * (pi / (2 * MEMBRANE_RADIUS)) \
+            * sin(pi * x / (2 * MEMBRANE_RADIUS));
+    } else {
+        return 0;
+    }
+}
+
+double membrane_second_derivative(double x) {
+/* Continuous function for the second derivative of the membrane position */
+    if (x <= MEMBRANE_RADIUS) {
+        return -mag * pow(pi / (2 * MEMBRANE_RADIUS), 2) \
+            * cos(pi * x / (2 * MEMBRANE_RADIUS));
+    } else {
+        return 0;
+    }
+}
+
+
+static double droplet_phi (creal xy[2]) {
+/* Level-set function for the initial position of the droplet, where xy is an 
+array with xy[0] = x and xy[1] = y */
+  return sq(xy[0]) + sq(xy[1] - membrane_position(xy[0]) - drop_centre) \
+    - sq(DROP_RADIUS);
+}
+
+
+static void vofi (scalar c, int levelmax) {
+/* vofi function to define the free surface of the droplet */
+  double fh = Get_fh (droplet_phi, NULL, BOX_WIDTH / (1 << levelmax), dimension, 0);
+  foreach() {
+    creal xy[2] = {x - Delta/2., y - Delta/2.};
+    c[] = Get_cc (droplet_phi, xy, Delta, fh, dimension);
+  }
+}
+
+
+int main() {
+
+    drop_centre = INITIAL_DROP_HEIGHT + DROP_RADIUS;
+
+
+    for (int refineLevel = MINLEVEL; refineLevel <= MAXLEVEL; refineLevel++) {
+        fprintf(stderr, "refineLevel = %d\n", refineLevel);
+
+        init_grid(1 << MINLEVEL); // Create grid according to the minimum level
+        size(BOX_WIDTH); // Size of the domain
+
+        scalar c[], kappa[], kappax[], kappay[];
+        c.refine = c.prolongation = fraction_refine;
+        c.height = htest;
+
+        /* Define the volume fraction and the curvature */
+        vofi (c, MINLEVEL);
+        for (int l = MINLEVEL + 1; l <= refineLevel; l++) {
+            refine (c[] > 0. && c[] < 1. && level < l);
+            refine((sq(x) + sq(y - membrane_position(x) - drop_centre) < sq(DROP_RADIUS + DROP_REFINED_WIDTH)) \
+                && (sq(x) + sq(y - membrane_position(x) - drop_centre)  > sq(DROP_RADIUS - DROP_REFINED_WIDTH)) \
+                && (level < refineLevel));
+            vofi (c, l);
+        }
+        
+        /* Set fields for membrane position */
+        foreach() { 
+            W[] = membrane_position(x);
+            Wx[] = membrane_first_derivative(x);
+            Wxx[] = membrane_second_derivative(x);
+        }
+
+        /* Determine the curvature and heights */
+        heights(c, htest);
+        cstats s = curvature (c, kappa, sigma = 2., add = false);
+        foreach() { 
+            if (c[] == 0 || c[] == 1) {
+                kappax[] = nodata;
+                kappay[] = nodata;
+            } else {
+                double kappaxVal = kappa_x(point, htest);
+                double kappayVal = kappa_y(point, htest);
+
+                if (fabs(kappaxVal) > 1e3) {
+                    kappax[] = nodata;
+                } else {
+                    kappax[] = 2 * fabs(kappaxVal);
+                }
+
+                if (fabs(kappayVal) > 1e3) {
+                    kappay[] = nodata;
+                } else {
+                    kappay[] = 2 * fabs(kappayVal);
+                }
+            }
+        }
+
+        /* Output gfs and curvature files */
+        char gfs_filename[80];
+        sprintf(gfs_filename, "gfs_output_%d.gfs", refineLevel);
+        output_gfs(file = gfs_filename);
+
+
+        /* Output the interface and curvature along it */
+        char interface_filename[80];
+        sprintf(interface_filename, "interface_%d.txt", refineLevel);
+        FILE *interface_file = fopen(interface_filename, "w");
+
+        foreach() {
+            if (c[] > 1e-6 && c[] < 1. - 1e-6) {
+
+                // Height function derivatives
+                double hx = (htest.y[1, 0] - htest.y[-1, 0])/2.;
+                double hxx = (htest.y[1, 0] + htest.y[-1, 0] - 2.*htest.y[])/Delta;
+                double hy = (htest.x[0, 1] - htest.x[0, -1])/2.;
+                double hyy = (htest.x[0, 1] + htest.x[0, -1] - 2.*htest.x[])/Delta;
+
+                // Segment info
+                coord n = interface_normal(point, c);
+                double alpha = plane_alpha(c[], n);
+                coord segment[2];
+                if (facets(n, alpha, segment) == 2) {
+                    fprintf(interface_file, \
+                        "%.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f\n", 
+                        x + segment[0].x * Delta, y + segment[0].y * Delta,
+                        x + segment[1].x * Delta, y + segment[1].y * Delta,
+                        kappa[], kappax[], kappay[],
+                        htest.y[], hx, hxx, 
+                        htest.x[], hy, hyy,
+                        W[], Wx[], Wxx[], 
+                        x, y);
+                }
+            }
+        }
+        fclose(interface_file);
+    }
+
+    
+
+    
+
+    
+
+}
+
---
+++ basilisk_scripts/utility_scripts/curvature_refinement_study.m
@@ -0,0 +1,205 @@
+%% curvature_refinement_study.m
+% Validation of the curvature by altering the refinement level
+
+clear;
+close all;
+
+% Radius and centre of the droplet in the lab frame
+R = 1;
+Yc = 0.125 + R;
+
+% Membrane parameters
+L = 1.25; % Width of membrane
+
+% Range of levels
+levels = 5 : 12;
+
+% Set up figure
+close all;
+figure(1);
+hold on;
+
+%% Loop over levels
+for shape = ["flat", "curved"]
+    
+    if shape == "flat"
+        mag = 0;
+    else
+        mag = 0.5;
+    end
+    
+    % Set W solution
+    WExact = @(X) mag * cos(pi * X / (2 * L)); 
+    WxExact = @(X) -(pi / (2 * L)) * mag * sin(pi * X / (2 * L));
+    WxxExact = @(X) -(pi / (2 * L))^2 * mag * cos(pi * X / (2 * L));
+    
+    % L2 norm error arrays
+    lineCentredL2Norms = zeros(size(levels));
+    cellCentredL2Norms = zeros(size(levels));
+    for levelIdx = 1 : length(levels)
+        level = levels(levelIdx);
+
+        % Load in output file
+        outputFilename = sprintf("refinement_data_%s/interface_%d.txt", shape, level);
+        A = readmatrix(outputFilename);
+
+        % Set constants related to data size
+        noSegments = size(A, 1) % Number of line segments
+        noVariables = size(A, 2) - 4; % Number of variables (e.g. curvature) 
+
+        % Initialise the matrix B, which will contain the data from A but sorted so
+        % that the line segments connect to each other
+        B = zeros(size(A));
+
+        %% Finding the first element
+        % Here we move the elements of A into B, sorting such that the line
+        % segments in consecutive rows are connected
+
+        % Parameters used in the algorithm
+        tol = 1e6; % Tolerance to say two points are close (depends on level)
+        hugeVal = 1e5; % Huge value for distance calculations
+
+        % Find the index of the segment with the largest value of y (start or end
+        % point)
+        [maxStart, startIdx] = max(A(:, 2));
+        [maxEnd, endIdx] = max(A(:, 4));
+        if (maxStart > maxEnd)
+            initIdx = startIdx;
+        else
+            initIdx = endIdx;
+        end
+
+        % Copy the row from A over to B, and replace the row in A
+        B(1, :) = A(initIdx, :);
+        A(initIdx, :) = hugeVal * ones(1, noVariables + 4);
+
+        %% Sort the remaining rows 
+        startDists = zeros(noSegments); % Intialise distance array
+        currPoint = B(1, 3 : 4); % Current point 
+
+        for k = 2 : noSegments
+            k
+
+            % Find distances of the start and end points in A form currPoint
+            startDists = sqrt((currPoint(1) - A(:, 1)).^2 + (currPoint(2) - A(:, 2)).^2);
+            endDists = sqrt((currPoint(1) - A(:, 3)).^2 + (currPoint(2) - A(:, 4)).^2);
+
+            % Find the minimum distance
+            [minStart, startIdx] = min(startDists);
+            [minEnd, endIdx] = min(startDists);
+            minVal = min([minStart, minEnd]);
+
+            if (minStart < minEnd)
+                % If the closest point is a start point, then update the new
+                % currPoint to be the end point of the current segment
+                idx = startIdx;
+                currPoint = A(idx, 3 : 4);
+            else
+                % Else the closest point is an end point, so update the new
+                % currPoint to be the start point of the current segment
+                idx = endIdx;
+                currPoint = A(idx, 1 : 2);
+            end
+        %     
+
+            % If minVal is larger than tol, then quit
+            if (minVal > tol)
+                disp("minVal = " + minVal);
+                error("minVal is too large");
+            end
+
+            % Copy the row over to B and replace the row in A
+            B(k, :) = A(idx, :);
+            A(idx, :) = hugeVal * ones(1, noVariables + 4);
+        end
+
+
+        %% Determine arc length
+        segmentLengths = sqrt((B(:, 1) - B(:, 3)).^2 + (B(:, 2) - B(:, 4)).^2);
+        arcLengths = cumtrapz(segmentLengths);
+
+        %% Load in variables
+        xStarts = B(:, 1);
+        yStarts = B(:, 2);
+        xEnds = B(:, 3);
+        yEnds = B(:, 4);
+        kappa = B(:, 5);
+        kappax = B(:, 6);
+        kappay = B(:, 7);
+        heightY = B(:, 8);
+        hx = B(:, 9);
+        hxx = B(:, 10);
+        heightX = B(:, 11);
+        hy = B(:, 12);
+        hyy = B(:, 13);
+        W = B(:, 14);
+        Wx = B(:, 15);
+        Wxx = B(:, 16);
+        xCellCentres = B(:, 17);
+        yCellCentres = B(:, 18);
+
+        % Convert large values to NaN
+        kappax(kappax > 1e3) = nan;
+        kappay(kappay > 1e3) = nan;
+        hx(abs(hx) > 1e3) = nan;
+        hy(abs(hy) > 1e3) = nan;
+        hxx(abs(hxx) > 1e3) = nan;
+        hyy(abs(hyy) > 1e3) = nan;
+
+%         close all;
+
+        %% Plot curvature
+%         subplot(2,1,1);
+%         scatter(arcLengths, kappa, [], [0.9290, 0.6940, 0.1250]);
+%         grid on
+%         legend("kappa", "interpreter", "latex", "Fontsize", 15);
+%         xlabel("Arc length", "interpreter", "latex", "Fontsize", 18);
+%         ylabel("Curvature", "interpreter", "latex", "Fontsize", 18);
+%         set(gca, "ticklabelinterpreter", "latex", "Fontsize", 15);
+%     %     ylim([1.997, 2.003]);
+%         title("Comparison of curvature", "interpreter", "latex", "Fontsize", 18);
+% 
+%         subplot(2,1,2); 
+%         hold on;
+%         scatter(arcLengths, kappax);
+%         scatter(arcLengths, kappay);
+%         grid on
+%         legend(["kappa\_x", "kappa\_y"], "interpreter", "latex", "Fontsize", 15);
+%         xlabel("Arc length", "interpreter", "latex", "Fontsize", 18);
+%         ylabel("Curvature", "interpreter", "latex", "Fontsize", 18);
+%         set(gca, "ticklabelinterpreter", "latex", "Fontsize", 15);
+%     %     ylim([1.997, 2.003]);
+%         set(gcf, 'position', [200 200 1200 800]);
+%         pause(1);
+
+        %% Find centres of segments
+        % Different to xCentres and yCentres, which are the x and y coordinates of
+        % the centre of the CELL, not the segment
+        xLineCentres = 0.5 * (xStarts + xEnds);
+        yLineCentres = 0.5 * (yStarts + yEnds);
+
+        %% Determine kappax in the cell centred or line centred formulation
+        % Cell centred
+        kappax_cellCentred = 2 * abs((hyy + hy.^3 .* WxxExact(xCellCentres)) ...
+            ./ ((1 - WxExact(xCellCentres) .* hy).^2 + hy.^2).^(3/2));
+        kappax_cellCentred(kappa == kappay) = nan;
+
+        kappax_lineCentred = 2 * abs((hyy + hy.^3 .* WxxExact(xLineCentres)) ...
+            ./ ((1 - WxExact(xLineCentres) .* hy).^2 + hy.^2).^(3/2));
+        kappax_lineCentred(kappa == kappay) = nan;
+
+        %% Determine L2-norm errors
+        cellCentredL2Norms(levelIdx) = sqrt(sum((kappax_cellCentred - 2).^2, 'omitnan') / noSegments);
+        lineCentredL2Norms(levelIdx) = sqrt(sum((kappax_lineCentred - 2).^2, 'omitnan') / noSegments)
+
+    end
+
+
+
+    %% Plot L2-norm errors
+    plot(levels, cellCentredL2Norms, 'Displayname', sprintf("Cell centred (%s)", shape));
+    plot(levels, lineCentredL2Norms, 'Displayname', sprintf("Line centred (%s)", shape));
+
+end
+set(gca, 'yscale', 'log');
+legend();
