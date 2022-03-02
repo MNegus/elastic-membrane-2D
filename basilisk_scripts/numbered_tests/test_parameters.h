569ffc600e10b7132099b66bf9db16a90f6cc647
MNegus <michaelnegus1@gmail.com>
2022-03-02 07:45:24 +0000

    Removed membrane radius, as is now defined in script

--- basilisk_scripts/numbered_tests/test_parameters.h
+++ basilisk_scripts/numbered_tests/test_parameters.h
@@ -20,7 +20,6 @@ const double INITIAL_DROP_HEIGHT = 0.125; // Initial gap between drop and plate
 const int MINLEVEL = 5; // Minimum refinement level 
 const int MAXLEVEL = 10; // Maximum refinement level
 const double BOX_WIDTH = 6.0; // Width of the computational box
-const double MEMBRANE_RADIUS = 3.0; // Width of the membrane
 const double GFS_OUTPUT_TIMESTEP = 1e-2; // Time between gfs outputs
 const double PLATE_OUTPUT_TIMESTEP = 1e-4; // Time between plate outputs
 const double LOG_OUTPUT_TIMESTEP = 1e-4; // Time between log outputs
